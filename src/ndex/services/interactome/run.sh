ddc0b320f7de0bebe79710e92088bd4f49c83ea0
Chris Churas <churas.camera@gmail.com>
2020-01-14 16:58:07 -0800

    set host to localhost

--- src/ndex/services/interactome/run.sh
+++ src/ndex/services/interactome/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #rest service will be on the default port 8285
-nohup java -Xmx1g -Dndex.host="http://test.ndexbio.org/v2" -Dndex.interactomehost=test.ndexbio.org -Dndex.interactomedb=/opt/ndex/services/interactome -Dndex.queryport=8287 -jar @@INTERACTOME_JAR@@ & 1>out
+nohup java -Xmx1g -Dndex.host="http://localhost/v2" -Dndex.interactomehost=localhost -Dndex.interactomedb=/opt/ndex/services/interactome -Dndex.queryport=8287 -jar @@INTERACTOME_JAR@@ & 1>out

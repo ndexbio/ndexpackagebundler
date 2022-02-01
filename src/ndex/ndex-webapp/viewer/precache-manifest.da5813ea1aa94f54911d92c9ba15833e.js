ab710612fb3483d7cdf23c1a8a666dd7ffb44e62
Chris Churas <churas.camera@gmail.com>
2022-02-01 12:24:01 -0800

    initial version

---
+++ src/ndex/ndex-webapp/viewer/precache-manifest.da5813ea1aa94f54911d92c9ba15833e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd2f7845e2fa230924d3ba02767e358",
+    "url": "/viewer/index.html"
+  },
+  {
+    "revision": "d3bd0fa5326c7711672d",
+    "url": "/viewer/static/css/main.653bd8d0.chunk.css"
+  },
+  {
+    "revision": "202eb5207fced3005e08",
+    "url": "/viewer/static/js/2.a86a6141.chunk.js"
+  },
+  {
+    "revision": "14337ad9c19e9469a781e8699f176c27",
+    "url": "/viewer/static/js/2.a86a6141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3bd0fa5326c7711672d",
+    "url": "/viewer/static/js/main.507914d0.chunk.js"
+  },
+  {
+    "revision": "b7fb2d1df9643eb5e00ab3a0b64b1054",
+    "url": "/viewer/static/js/main.507914d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c28cca9767ae9c0c8542",
+    "url": "/viewer/static/js/runtime-main.87274826.js"
+  },
+  {
+    "revision": "04d7bf44735425feeae95497cf4019b6",
+    "url": "/viewer/static/media/ndex-logo.04d7bf44.svg"
+  }
+]);
